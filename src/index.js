20604e997280eb3bfcfa52a7da84cf88eb3f16ef
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2021-01-26 18:09:29 +0100

    complete props section

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="My first blog post!" avatar={faker.image.image()}/>
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" commentText="Very nice blog post." avatar={faker.image.image()}/>
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="The final blog post." avatar={faker.image.image()}/>
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="My first blog post!" avatar={faker.image.image()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 2:00AM" commentText="Very nice blog post." avatar={faker.image.image()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="The final blog post." avatar={faker.image.image()}/>
+      </ApprovalCard>
     </div>
   )
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
-
-{/* <img alt="avatar" src='https://source.unsplash.com/random' /> */}
